d2cc072e92c79a8664219b49560a02515612e682
Roberto Vasquez Angel <robotex@robotex.de>
2012-08-20 16:44:05 +0200

    Added twitter bootstrap to dummy app.

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -4,6 +4,8 @@ Dummy::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   
   Ecm::Products::Routing.routes(self)
+  
+  root :to => 'home#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
